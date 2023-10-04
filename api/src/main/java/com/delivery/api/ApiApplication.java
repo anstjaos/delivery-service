f5c6a06c7f7aa75d8e24338b792acb82787459ac
anstjaos <vv_eros@naver.com>
2023-10-04 12:17:25 +0900

    API 모듈 설정

--- api/src/main/java/com/delivery/api/ApiApplication.java
+++ api/src/main/java/com/delivery/api/ApiApplication.java
@@ -1,13 +1,11 @@
-package com.delivery.service;
+package com.delivery.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DeliveryServiceApplication {
-
+public class ApiApplication {
     public static void main(String[] args) {
-        SpringApplication.run(DeliveryServiceApplication.class, args);
+        SpringApplication.run(ApiApplication.class, args);
     }
-
 }
