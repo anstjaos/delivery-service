0eaa27d3c5a445bd257d888e2a3ff84167b1bd92
anstjaos <vv_eros@naver.com>
2023-10-08 16:49:51 +0900

    Exception handler 추가

---
+++ api/src/main/java/com/delivery/api/common/exception/ApiException.java
@@ -0,0 +1,35 @@
+package com.delivery.api.common.exception;
+
+import com.delivery.api.common.error.ErrorCodeInterface;
+import lombok.Getter;
+
+@Getter
+public class ApiException extends RuntimeException implements ApiExceptionInterface {
+
+    private final ErrorCodeInterface errorCodeInterface;
+    private final String errorDescription;
+
+    public ApiException(ErrorCodeInterface errorCodeInterface) {
+        super(errorCodeInterface.getDescription());
+        this.errorCodeInterface = errorCodeInterface;
+        this.errorDescription = errorCodeInterface.getDescription();
+    }
+
+    public ApiException(ErrorCodeInterface errorCodeInterface, String errorDescription) {
+        super(errorDescription);
+        this.errorCodeInterface = errorCodeInterface;
+        this.errorDescription = errorDescription;
+    }
+
+    public ApiException(ErrorCodeInterface errorCodeInterface, Throwable throwable) {
+        super(throwable);
+        this.errorCodeInterface = errorCodeInterface;
+        this.errorDescription = errorCodeInterface.getDescription();
+    }
+
+    public ApiException(ErrorCodeInterface errorCodeInterface, Throwable throwable, String errorDescription) {
+        super(throwable);
+        this.errorCodeInterface = errorCodeInterface;
+        this.errorDescription = errorDescription;
+    }
+}
---
+++ api/src/main/java/com/delivery/api/common/exception/ApiExceptionInterface.java
@@ -0,0 +1,9 @@
+package com.delivery.api.common.exception;
+
+import com.delivery.api.common.error.ErrorCodeInterface;
+
+public interface ApiExceptionInterface {
+
+    ErrorCodeInterface getErrorCodeInterface();
+    String getErrorDescription();
+}
