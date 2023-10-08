4520952265e2ea8c1c6ca6a8a78fcef9c34f451e
anstjaos <vv_eros@naver.com>
2023-10-08 17:01:23 +0900

    Interceptor 추가

---
+++ api/src/main/java/com/delivery/api/config/web/WebConfig.java
@@ -0,0 +1,36 @@
+package com.delivery.api.config.web;
+
+import com.delivery.api.interceptor.AuthorizationInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthorizationInterceptor authorizationInterceptor;
+
+    private final List<String> OPEN_API = List.of("/open-api/**");
+    private final List<String> DEFAULT_EXCLUDE = List.of(
+            "/",
+            "favicon.ico",
+            "/error"
+    );
+    private final List<String> SWAGGER = List.of(
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/v3/api-docs/**"
+    );
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authorizationInterceptor)
+                .excludePathPatterns(OPEN_API)
+                .excludePathPatterns(DEFAULT_EXCLUDE)
+                .addPathPatterns(SWAGGER);
+    }
+}
