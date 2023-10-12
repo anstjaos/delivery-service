7adc47ccd4f66a5d8e7c3f9eb6b13f20d234a216
anstjaos <vv_eros@naver.com>
2023-10-12 21:34:44 +0900

    Store 도메인 추가

---
+++ db/src/main/java/com/delivery/db/store/StoreRepository.java
@@ -0,0 +1,17 @@
+package com.delivery.db.store;
+
+import com.delivery.db.store.enums.StoreCategory;
+import com.delivery.db.store.enums.StoreStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface StoreRepository extends JpaRepository<StoreEntity, Long> {
+
+    Optional<StoreEntity> findFirstByIdAndStatusOrderByIdDesc(Long id, StoreStatus status);
+
+    List<StoreEntity> findAllByStatusOrderByIdDesc(StoreStatus status);
+
+    List<StoreEntity> findAllByStatusAndCategoryOrderByStarDesc(StoreStatus status, StoreCategory category);
+}
