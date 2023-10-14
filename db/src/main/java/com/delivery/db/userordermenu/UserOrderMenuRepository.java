f5da346d85091d9a0cbdd16ee87e4f8680c550d9
anstjaos <vv_eros@naver.com>
2023-10-14 14:54:21 +0900

    User order, user order menu 서비스 로직 추가

--- db/src/main/java/com/delivery/db/userordermenu/UserOrderMenuRepository.java
+++ db/src/main/java/com/delivery/db/userordermenu/UserOrderMenuRepository.java
@@ -1,6 +1,10 @@
 package com.delivery.db.userordermenu;
 
+import com.delivery.db.userordermenu.enums.UserOrderMenuStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserOrderMenuRepository extends JpaRepository<UserOrderMenuEntity, Long> {
+    List<UserOrderMenuEntity> findAllByUserOrderIdAndStatus(Long userOrderId, UserOrderMenuStatus status);
 }
