4c96a61252300b1aa2a509d352e3254ed65c6f8e
anstjaos <vv_eros@naver.com>
2023-10-15 14:12:01 +0900

    store user 엔티티 추가

---
+++ db/src/main/java/com/delivery/db/storeuser/StoreUserRepository.java
@@ -0,0 +1,11 @@
+package com.delivery.db.storeuser;
+
+import com.delivery.db.storeuser.enums.StoreUserStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface StoreUserRepository extends JpaRepository<StoreUserEntity, Long> {
+
+    Optional<StoreUserEntity> findFirstByEmailAndStatusOrderByIdDesc(String email, StoreUserStatus status);
+}
---
+++ db/src/main/java/com/delivery/db/storeuser/enums/StoreUserStatus.java
@@ -0,0 +1,12 @@
+package com.delivery.db.storeuser.enums;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum StoreUserStatus {
+
+    REGISTERED("등록"),
+    UNREGISTERED("해지");
+
+    private final String description;
+}
